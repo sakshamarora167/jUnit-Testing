80dc5b28d1c84d22ac55eb8a9ef2390df7931d82
saksham.arora <saksham.arora@CSELAB02-06.bennett.edu.in>
2017-09-26 12:51:49 +0530

    lab_7

---
+++ src/jUnitTestingPackage/jUnitFunction.java
@@ -0,0 +1,10 @@
+package jUnitTestingPackage;
+
+public class jUnitFunction {
+	public int addNumbers(int num1, int num2){
+		return num1 +num2;
+	}
+	public String addStrings(String x, String y){
+		return x+y;
+	}
+}
